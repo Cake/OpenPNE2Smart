c50ed1b9ee616a5c1f6ee3e00cf677e3a322338e
Cake <cake_67@users.sourceforge.jp>
2012-06-04 16:01:45 +0900

    Common Error for Smartphone regist

---
+++ webapp/modules/pc/page/o_regist_error.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright 2005-2008 OpenPNE Project
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+
+/* OpenPNE2 スマートフォン対応：独自アクション */
+class pc_page_o_regist_error extends OpenPNE_Action
+{
+    function isSecure()
+    {
+        return false;
+    }
+
+    function execute($requests)
+    {
+
+        //---- inc_ テンプレート用 変数 ----//
+        $this->set('inc_page_header', fetch_inc_page_header('public'));
+
+        $this->set('SNS_NAME', SNS_NAME);
+
+        return 'success';
+    }
+}
+?>
---
+++ webapp/modules/smart/templates/o_regist_error.tpl
@@ -0,0 +1,15 @@
+<div id="LayoutC">
+<div id="Center">
+({* {{{ formTable *})
+<div class="dparts formTable"><div class="parts">
+<div class="partsHeading"><h3>登録できません</h3></div>
+<div class="partsInfo">
+<p>
+現在、スマートフォンでの登録はできません。
+</p>
+</div>
+</div></div>
+({* }}} *})
+
+</div><!-- Center -->
+</div><!-- LayoutC -->
