71c31a5d8af9465204ce38a50679aaf22b90b792
Cake <cake_67@users.sourceforge.jp>
2012-06-05 14:11:00 +0900

    Regist ktai_address for Smart phone SLAVEPNE login

--- webapp/modules/pc/do/h_regist_address.php
+++ webapp/modules/pc/do/h_regist_address.php
@@ -30,7 +30,18 @@ class pc_do_h_regist_address extends OpenPNE_Action
             openpne_redirect('pc', 'page_h_regist_address', $p);
         }
 
-        $c_member_id = db_member_c_member_id4pc_address($pc_address);
+        /* OpenPNE2 スマートフォン対応：ここから */
+//        $c_member_id = db_member_c_member_id4pc_address($pc_address);
+        $is_ktai_address = false;
+        $smartPhone = new OpenPNE_SmartPhoneUA();
+        if ($smartPhone->is_smart && is_ktai_mail_address($pc_address)) {
+            $c_member_id = db_member_c_member_id4ktai_address($pc_address);
+            $is_ktai_address = true;
+        } else {
+            $c_member_id = db_member_c_member_id4pc_address($pc_address);
+        }
+        /* OpenPNE2 スマートフォン対応：ここまで */
+
         if ($c_member_id == $u) {
             //自分のメールアドレス
             $p = array('msg' => "入力されたメールアドレスは既に登録されています");
@@ -47,12 +58,28 @@ class pc_do_h_regist_address extends OpenPNE_Action
             openpne_redirect('pc', 'page_h_regist_address', $p);
         }
 
-        if (is_ktai_mail_address($pc_address)) {
+        /* OpenPNE2 スマートフォン対応：ここから */
+//        if (is_ktai_mail_address($pc_address)) {
+        if (is_ktai_mail_address($pc_address) && !$is_ktai_address) {
             $p = array('msg' => '携帯メールアドレスは記入できません');
             openpne_redirect('pc', 'page_h_regist_address', $p);
         }
 
-        db_member_h_regist_mail($u, $pc_address);
+        if ($is_ktai_address) {
+            db_member_delete_c_ktai_address_pre4ktai_address($pc_address);
+
+            $session = create_hash();
+            db_member_insert_c_ktai_address_pre($u, $session, $pc_address);
+
+            //function cache削除
+            cache_drop_c_member_profile($u);
+
+            do_mail_sns_change_ktai_mail_send($u, $session, $pc_address);
+        } else {
+            db_member_h_regist_mail($u, $pc_address);
+        }
+//        db_member_h_regist_mail($u, $pc_address);
+        /* OpenPNE2 スマートフォン対応：ここまで */
 
         $GLOBALS['AUTH']->logout();
         openpne_redirect('pc', 'page_o_h_regist_mail');
