1f2cf755796e693b99ae589ab07d156ef3a3ef57
Cake <cake_67@users.sourceforge.jp>
2012-06-13 16:21:15 +0900

    Add CSS for Smartphone

---
+++ public_html/smart_style.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/css');
+
+header('Cache-Control: max-age=315360000');
+header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('+10 years')) . ' GMT');
+?>
+@charset "UTF-8";
+
--- webapp/lib/smarty_plugins/function.t_url_smart_style.php
+++ webapp/lib/smarty_plugins/function.t_url_smart_style.php
@@ -10,6 +10,8 @@ function smarty_function_t_url_smart_style($params, &$smarty)
 	<link rel="stylesheet" href="./modules/smart/jquermobile/1.1/jquery.mobile.min.css">
 	<script type="text/javascript" src="./modules/smart/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="./modules/smart/jquermobile/1.1/jquery.mobile.min.js"></script>
+<link rel="stylesheet" href="./smart_style.php?hash={$hash}" type="text/css" />
+EOD;
 EOD;
 
     return $result;
